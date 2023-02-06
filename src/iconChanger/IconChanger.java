68ccc494d1ca352aac7fff9b9f25434284ff9f38
yeyroy <yeyroy@gmail.com>
2023-02-05 22:33:34 -0700

    initial commit

---
+++ src/iconChanger/IconChanger.java
@@ -0,0 +1,21 @@
+package iconChanger;
+
+public class IconChanger {
+
+	private static String discordToken;
+	private static String twitchToken;
+	
+	public static void main(String[] args) {
+		
+		loadCredentials();
+		//ApuHug
+		
+		
+	}
+	
+	private static void loadCredentials() {
+		//load discord and twitch credentials
+		
+	}
+	
+}
---
+++ src/iconChanger/Server.java
@@ -0,0 +1,5 @@
+package iconChanger;
+
+public class Server {
+
+}
